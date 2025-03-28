c0df7966c3bb039478d8c1acc26ffba1daf41e65
Jose-Simon <josesimonh@gmail.com>
2025-03-28 11:55:12 -0400

    Update settings.py to include SQLite

--- dejavu/config/settings.py
+++ dejavu/config/settings.py
@@ -27,7 +27,8 @@ OFFSET_SECS = 'offset_seconds'
 # DATABASE CLASS INSTANCES:
 DATABASES = {
     'mysql': ("dejavu.database_handler.mysql_database", "MySQLDatabase"),
-    'postgres': ("dejavu.database_handler.postgres_database", "PostgreSQLDatabase")
+    'postgres': ("dejavu.database_handler.postgres_database", "PostgreSQLDatabase"),
+    'sqlite3': ("dejavu.database_handler.sqlite3_database", "Sqlite3Database"),
 }
 
 # TABLE SONGS
