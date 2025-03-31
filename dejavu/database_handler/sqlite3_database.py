1e46c9396a6169a0452a76e6984b28ee17a366be
Jose-Simon <josesimonh@gmail.com>
2025-03-31 02:13:53 -0400

    Update sqlite3_database.py Add total_hashes in insert_song function

--- dejavu/database_handler/sqlite3_database.py
+++ dejavu/database_handler/sqlite3_database.py
@@ -101,7 +101,7 @@ class Sqlite3Database(CommonDatabase):
         """
         with self.cursor() as cur:
             cur.execute(
-                "INSERT INTO songs (name, file_path, file_hash, fingerprinted) VALUES (?, ?, ?, 0)",
+                "INSERT INTO songs (name, file_path, file_hash, total_hashes, fingerprinted) VALUES (?, ?, ?, 0)",
                 (song_name, file_path or song_name, file_hash)  # using song_name as a proxy for full path
             )
             return cur.lastrowid
